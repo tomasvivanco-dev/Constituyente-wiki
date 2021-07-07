9f71305abdb37008479a1aa8d507618f98f32ec0
tomasvivanco-dev <67025974+tomasvivanco-dev@users.noreply.github.com>
2021-07-07 14:21:17 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Convención.wiki',
+  tagline: 'Depositario ciudadano de la Convención Constituyente de Chile',
+  url: 'https://constituyente.wiki',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
@@ -9,9 +9,9 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Convención.wiki',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Logo',
         src: 'img/logo.svg',
       },
       items: [
