4e095af347cbc56f3de52efd58957d015349c0e9
tomasvivanco-dev <67025974+tomasvivanco-dev@users.noreply.github.com>
2021-07-08 14:21:19 -0400

    más alternativas de web

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    Cronica: ['doc1', 'doc2', 'doc3', 'doc4'],
+    Videos: ['video1'],
   },
 };
